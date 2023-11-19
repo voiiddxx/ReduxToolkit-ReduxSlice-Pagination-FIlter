80419f7f95991164190a736f20422f9ea5c18ac9
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-11-19 14:22:17 +0530

    added  user page ui

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./User.css"
-
 const User = () => {
+    const [pageNo, setpageNo] = useState(1);
+    
+
   return (
     <div className="user-section">
         <div className="user-heading">
@@ -10,7 +12,69 @@ const User = () => {
         </div>
 
         <div className="user-table">
-            <div className="row"></div>
+            <div className="row">
+                <img src="https://www.shutterstock.com/image-photo/young-mixed-race-woman-isolated-260nw-1960001155.jpg" alt="thisisimageofperson" />
+                <h2>Alex carry</h2>
+                <p>Finance</p>
+                <p>Female</p>
+                <p>Available</p>
+            </div>
+            <div className="row">
+                <img src="https://www.shutterstock.com/image-photo/young-mixed-race-woman-isolated-260nw-1960001155.jpg" alt="thisisimageofperson" />
+                <h2>Alex carry</h2>
+                <p>Finance</p>
+                <p>Female</p>
+                <p>Available</p>
+            </div>
+            <div className="row">
+                <img src="https://www.shutterstock.com/image-photo/young-mixed-race-woman-isolated-260nw-1960001155.jpg" alt="thisisimageofperson" />
+                <h2>Alex carry</h2>
+                <p>Finance</p>
+                <p>Female</p>
+                <p>Available</p>
+            </div>
+            <div className="row">
+                <img src="https://www.shutterstock.com/image-photo/young-mixed-race-woman-isolated-260nw-1960001155.jpg" alt="thisisimageofperson" />
+                <h2>Alex carry</h2>
+                <p>Finance</p>
+                <p>Female</p>
+                <p>Available</p>
+            </div>
+            <div className="row">
+                <img src="https://www.shutterstock.com/image-photo/young-mixed-race-woman-isolated-260nw-1960001155.jpg" alt="thisisimageofperson" />
+                <h2>Alex carry</h2>
+                <p>Finance</p>
+                <p>Female</p>
+                <p>Available</p>
+            </div>
+            <div className="row">
+                <img src="https://www.shutterstock.com/image-photo/young-mixed-race-woman-isolated-260nw-1960001155.jpg" alt="thisisimageofperson" />
+                <h2>Alex carry</h2>
+                <p>Finance</p>
+                <p>Female</p>
+                <p>Available</p>
+            </div>
+        </div>
+        <div className="user-bottom">
+            <div className="page">
+                <div onClick={()=>{
+                    setpageNo(pageNo+1);
+                }} className="decrement">
+                    <h1>+</h1>
+                </div>
+                <div className="page_no">
+                    <h2>{pageNo}</h2>
+                </div>
+                <div onClick={()=>{
+                    if(pageNo>1){
+                        setpageNo(pageNo-1);
+                    }
+                }} className="increment">
+                    <h2>-</h2>
+                </div>
+                
+            </div>
+            <div className="filter"></div>
         </div>
     </div>
   )
