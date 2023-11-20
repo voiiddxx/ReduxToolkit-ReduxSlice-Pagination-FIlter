ebd0175b1ebe64a0b5dd4bd55b045f95f9ba7fbe
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-11-20 14:44:00 +0530

    Create user api integreated

--- src/View/createuser/Createuser.jsx
+++ src/View/createuser/Createuser.jsx
@@ -1,10 +1,56 @@
 import React, { useState } from 'react'
 import "./Createuser.css"
+import axios from "axios"
 
 const Createuser = ({close}) => {
+    const [first_name, setfirst_name] = useState("");
+    const [last_name, setlast_name] = useState("");
     const [Gender, setGender] = useState("");
     const [Domain, setDomain] = useState("");
     const [Available, setAvailable] = useState(null);
+    const [email, setemail] = useState("");
+    const [Image, setImage] = useState(null);
+    
+
+    const CreateUser = async (imageUrl)=>{
+        try {
+            const bodyParameter = {
+                first_name:first_name,
+                last_name:last_name,
+                email:email,
+                avatar:imageUrl,
+                gender:Gender,
+                domain:Domain,
+                available:Available
+            }
+            const response = await axios.post("https://heliverse-assignment-server.onrender.com/api/users" , bodyParameter);
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    
+
+    const handleSubmit = ()=>{
+        
+        try {
+            let formdata = new FormData();
+            formdata.append("file" , Image[0]);
+      formdata.append("upload_preset" , "qouutdij");
+      axios.post("https://api.cloudinary.com/v1_1/dwkmxsthr/upload" , formdata , {
+          onDownloadProgress:(ProgressEvent)=>{
+            console.log("Uploading..." ,Math.round( ProgressEvent.loaded/ProgressEvent.total));
+          }
+      }).then(response=>{
+        console.log(response.data.url);
+        console.log(first_name , last_name , email , Domain , Gender , Available);
+        CreateUser(response.data.url);
+    });
+        } catch (error) {
+            console.log(error);
+        }
+    }
     
   return (
    <>
@@ -14,9 +60,20 @@ const Createuser = ({close}) => {
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 
     <div className="create-user-form">
-        <input className='user-form' type="text" placeholder='First name' />
-        <input className='user-form' type="text" placeholder='Last Name' />
-        <input className='user-form' type="email" placeholder='Enter Your Email Address' />
+        <input onChange={(e)=>{
+            setfirst_name(e.target.value);
+        }} className='user-form' type="text" placeholder='First name' />
+
+        <input onChange={(e)=>{
+            setlast_name(e.target.value)
+        }} className='user-form' type="text" placeholder='Last Name' />
+
+
+        <input onChange={(e)=>{
+            setemail(e.target.value)
+        }} className='user-form' type="email" placeholder='Enter Your Email Address' />
+
+        
         <div className="gender">
            {
             Gender==="Male" ? <div className="selected-gender-box">
@@ -63,10 +120,12 @@ const Createuser = ({close}) => {
         }} className="select-image">
             <h3>Select Image</h3>
         </div>
-        <input className='image' type="file" hidden />
+        <input onChange={(e)=>{
+            setImage(e.target.files)
+        }} className='image' type="file" hidden />
     </div>
 
-    <div className="submit-button">
+    <div onClick={handleSubmit} className="submit-button">
         <h3>Create User</h3>
     </div>
    </div>
